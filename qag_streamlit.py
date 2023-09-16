024a3bd0a8928eab46e2a874d8166964f60f62b8
BluJz <115343325+BluJz@users.noreply.github.com>
2023-09-16 19:27:00 +0200

    Matplotlib chart

--- qag_streamlit.py
+++ qag_streamlit.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 from qag_preprocess import getQAGDataframe, displayData
 from datetime import datetime
+import matplotlib.pyplot as plt
+from streamlit_echarts import st_echarts
 
 # Load your DataFrame
 # df = pd.read_csv('your_dataframe.csv')  # If you saved your DataFrame to a CSV file
@@ -9,6 +11,39 @@ df = getQAGDataframe()
 
 # Create Streamlit app
 st.title('Questions Au Gouvernement (QAG) - XVème législature')
+
+# Use st.beta_columns to create two columns for the date inputs
+start_date, end_date = st.columns(2)
+
+# Date Range Filter
+with start_date:
+    start_date = st.date_input('Date min', df['Date'].min())
+with end_date:
+    end_date = st.date_input('Date max', df['Date'].max())
+
+# Convert the start_date and end_date to datetime objects
+start_date = datetime.combine(start_date, datetime.min.time())
+end_date = datetime.combine(end_date, datetime.min.time())
+
+# Filter the DataFrame based on selected date range
+filtered_df = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+
+# Create a row for the responding ministry filter
+with st.expander("Filter by Responding Ministry"):
+    selected_ministry = st.selectbox(
+        'Select Ministry', df['Responding_Ministry'].unique())
+
+
+# Group by 'Asking_Group' and count the number of questions per group
+group_counts = filtered_df['Asking_Group'].value_counts()
+
+# Create a bar chart to visualize the number of questions
+fig, ax = plt.subplots()
+group_counts.plot(kind='bar', ax=ax)
+ax.set_xlabel('Groupe politique')
+ax.set_ylabel('Nombre de questions posées')
+ax.set_title('QAG posées par groupe politiques')
+st.pyplot(fig)
 
 st.markdown("<hr>", unsafe_allow_html=True)
 
