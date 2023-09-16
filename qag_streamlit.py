c39fc7f809a22a303e2eb9a11c8b1f20a439a553
BluJz <115343325+BluJz@users.noreply.github.com>
2023-09-16 19:03:31 +0200

    QAG details with good format

--- qag_streamlit.py
+++ qag_streamlit.py
@@ -8,42 +8,29 @@ from datetime import datetime
 df = getQAGDataframe()
 
 # Create Streamlit app
-st.title('Question Files Viewer')
+st.title('Questions Au Gouvernement (QAG) - XVème législature')
+
+st.markdown("<hr>", unsafe_allow_html=True)
+
+st.header('Questions Au Gouvernement en détail')
 
 # Add filters for Asking_Group and Responding_Ministry
 asking_group_filter = st.selectbox(
-    'Select Asking Group:', df['Asking_Group'].unique())
+    'Sélectionnez le groupe politique de la QAG:', df['Asking_Group'].unique())
 ministry_filter = st.selectbox(
-    'Select Responding Ministry:', df['Responding_Ministry'].unique())
+    'Sélectionnez le ministère cible de la QAG:', df['Responding_Ministry'].unique())
 
 # Apply filters to the DataFrame
 filtered_df = df[(df['Asking_Group'] == asking_group_filter)
                  & (df['Responding_Ministry'] == ministry_filter)]
 
 displayed_df = displayData(filtered_df)
-# st.write('Displayed Data:')
-# table = st.table(displayed_df.drop(columns=['Discussion_Content']))
 
-# Display the filtered DataFrame
-# st.write('Filtered Data:')
-# table = st.table(filtered_df.drop(columns=['Discussion_Content']))
-
-# Add expander for discussion content
-# st.write('Discussion Content:')
-# selected_row = st.expander(
-#     'Click a row to view Discussion Content:', expanded=False)
-# if selected_row:
-#     row_index = selected_row.radio('Select a Row:', filtered_df.index)
-#     selected_content = filtered_df.at[row_index, 'Discussion_Content']
-#     st.write(selected_content)
-
+# List of expandable questions
 expanders = []
 for index, row in displayed_df.iterrows():
     expander = st.expander(
         f"{row['Displayed_Date']} - {row['Asking_Person_ID']}", expanded=False)
-    expander.write(row['Discussion_Content'])
+    expander.markdown(row['Discussion_Content'], unsafe_allow_html=True)
+#    expander.write(row['Discussion_Content'])
     expanders.append(expander)
-
-# Listen for row selection
-# selected_expander = st.selectbox(
-#     'Select a Row:', expanders, format_func=lambda expander: expander.title)
